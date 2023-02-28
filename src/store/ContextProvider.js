4eafa20317e40fe4aca9d811d51e4f2c427b2a7a
Komal Mandal <56351799+Komalone@users.noreply.github.com>
2023-02-28 22:13:20 +0530

    Add files via upload

--- src/store/ContextProvider.js
+++ src/store/ContextProvider.js
@@ -1,42 +1,41 @@
-import React, { useReducer } from "react";
+import React, { 
+    //useReducer,
+    useState
+} from "react";
 import CartContext from "./store-context";
 
-const defaultState={
-    items:[],
-    totalAmount:0
-}
-
-const cartReducer = (state, action) =>{
-    if(action.type === 'ADD'){
-        const updatedTotalAmount=state.totalAmount + action.item.price;
-        const updatedItem= state.items.concat(action.item);
-   
-        return{
-            items: updatedItem,
-            totalAmount: updatedTotalAmount
-        }
-    }
-    if(action.type === 'REMOVE'){
-    return state;
-    }
-return defaultState;
-};
-
 const StoreProvider=(props)=>{
-    const [cartState, dispatchAction] = useReducer(
-        cartReducer,defaultState);
+    const [items, setItem ]= useState([])
+    
+    const updatedTotalAmount = Number(items.price);
     
     const addToCartHandler=(item)=>{
-        dispatchAction({type: 'ADD', item: item});
+        fetch('https://crudcrud.com/api/ba7f3aaf421d44f0bf990e95e4de49ea/addTocart', {
+            method: 'POST',
+            body: JSON.stringify( item ),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        //const updatedItems= axios.post(`https://crudcrud.com/api/79d6cf68c8cf4086bd2e133d02a6a9df/cartitem`, JSON.stringify(item))
+       // const updatedItems= items.concat(item);
+       console.log([...items, item]); 
+       setItem([...items, item]);
+        
     };
     
     const removeFromCartHandler=(id)=>{
-        dispatchAction({type:'REMOVE', id:id});
-    }
+        console.log(id);
+        fetch(`https://crudcrud.com/api/ba7f3aaf421d44f0bf990e95e4de49ea/addTocart/${id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        })
+          //const updatedItems = items.filter(item => item.id !== id);
+          //const updatedItems= axios.delete(`https://crudcrud.com/api/4f6edc817d5142be9322bd7b657d767d/cartItem/}`,)
+          setItem(id)
+    };
 
     const cartContext = {
-        items: cartState.items,
-        totalAmount: cartState.totalAmount,
+        items:items ,
+        totalAmount: updatedTotalAmount,
         addItem: addToCartHandler,
         removeItem: removeFromCartHandler,
       };
